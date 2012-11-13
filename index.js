fbe77516d5212116f2afc232a2966a485a6f02cc
Avétis KAZARIAN <avetis.k@gmail.com>
2012-11-13 15:11:51 +0100

    fix jsdoc

--- index.js
+++ index.js
@@ -3,6 +3,14 @@
  */
 module.exports = defaults;
 
+/**
+ * Merge default values.
+ *
+ * @param {Object} dest
+ * @param {Object} defaults
+ * @return {Object}
+ * @api public
+ */
 function defaults (dest, defaults) {
   for (var prop in defaults) {
     if (! (prop in dest)) {
